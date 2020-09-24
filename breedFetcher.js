00a0507b5f5b2752da10655835bdc37a27362e82
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 14:40:01 +0000

    Printing description when given breed from CLI

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,17 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
-  console.log('err:', err);
-  // console.log('response:', response);
-  // console.log('body:', body);
+const getUserInput = () => {
+  return process.argv.slice(2);
+}
+
+const requestDesc = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
   const obj = JSON.parse(body);
-  console.log('desc:', obj[0].description);
+  return callback(obj[0].description);
+  });
+}
+
+const input = getUserInput();
+const printDesc  = requestDesc(input, (desc) => {
+  console.log(desc);
 });
