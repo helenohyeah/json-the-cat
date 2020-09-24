3e7d1dbac48cca3f5c9f2fcf0bea987c9b216dda
helenohyeah <ouyang.helen@gmail.com>
2020-09-24 14:51:57 +0000

    Completed breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,16 +2,27 @@ const request = require('request');
 
 const getUserInput = () => {
   return process.argv.slice(2);
-}
+};
 
 const requestDesc = (breed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
-  const obj = JSON.parse(body);
-  return callback(obj[0].description);
+    // error case
+    if (err) {
+      console.log(`ERROR: ${err.code}`);
+      process.exit();
+    }
+    // parse string result into an object
+    const result = JSON.parse(body);
+    // if input is invalid and no desc found
+    if (result.length === 0) {
+      console.log(`Sorry, no description was found for: ${breed}`);
+      process.exit();
+    }
+    return callback(result[0].description);
   });
-}
+};
 
 const input = getUserInput();
-const printDesc  = requestDesc(input, (desc) => {
+requestDesc(input, (desc) => {
   console.log(desc);
 });
